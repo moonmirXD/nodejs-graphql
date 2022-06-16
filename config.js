47156490dfbdcc6c9185632570b45af4c212b1a7
Ryan Arqueza <arquezaryan@gmail.com>
2022-06-16 14:48:20 +0800

    add config go h44444ck

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://sample:sample@cluster0.cmg7x.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'asdjaksdjkaskdjaksdjhiqwyhe8qhjweionasdklnaksldnklzxc123123',
+};
+
+// MONGODB: 'mongodb+srv://test:test@cluster0.zshjh.mongodb.net/merng?retryWrites=true&w=majority',
