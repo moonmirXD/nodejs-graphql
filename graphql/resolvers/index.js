2d81c000e98909ded96a7af93dd0bb41a1e485a8
Ryan Arqueza <arquezaryan@gmail.com>
2022-06-20 14:58:31 +0800

    add realtime data subscription using graphql

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -11,4 +11,7 @@ module.exports = {
     ...postResolvers.Mutation,
     ...commentResolvers.Mutation,
   },
+  Subscription: {
+    ...postResolvers.Subscription,
+  },
 };
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -38,6 +38,10 @@ module.exports = {
 
       const post = await newPost.save();
 
+      context.pubsub.publish('NEW_POST', {
+        newPost: post,
+      });
+
       return post;
     },
     deletePost: async (_, { postId }, context) => {
@@ -55,6 +59,11 @@ module.exports = {
       } catch (error) {
         throw new Error(error);
       }
+    },
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST'),
     },
   },
 };
